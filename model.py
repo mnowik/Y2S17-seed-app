ddc2e44a8a586d76905376416f9ef73c64e97a25
mnowik <nowicki.matthieu@gmail.com>
2017-08-01 10:48:51 +0300

    add login to the seed app

--- model.py
+++ model.py
@@ -1,5 +1,9 @@
+from flask_login import UserMixin
+
 from sqlalchemy import Column, DateTime, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 Base = declarative_base()
 
@@ -10,3 +14,21 @@ class YourModel(Base):
 
 # IF YOU NEED TO CREATE OTHER TABLE 
 # FOLLOW THE SAME STRUCTURE AS YourModel
+
+
+class User(UserMixin, Base):
+    __tablename__ = 'user'
+    id            = Column(Integer, primary_key=True)
+    email         = Column(String)
+    pw_hash       = Column(String)
+    authenticated = Column(Boolean, default=False)
+
+    def __repr__(self):
+      return "<User: %s, password: %s>" % (
+        self.email, self.pw_hash)
+
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
